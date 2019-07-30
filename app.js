e03d6b6fb20ec9cbd90d0532000a4668158f9058
lazavazav <lazav2021@gmail.com>
2019-07-29 20:23:16 -0400

    changed secure from false to true

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ app.post('/send', (req, res) => {
 
       let transporter = nodemailer.createTransport({
         host: "smtp.mailgun.org",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
           user: process.env.USER, 
           pass: process.env.PASS  
